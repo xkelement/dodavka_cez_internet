9b513328f482c28b30b09f4f98b942cc09b975c9
Tomáš Kelemen <tk.kelemen@gmail.com>
2012-12-11 11:44:00 +0100

    zaciatok prece na grafike

--- projekt.c
+++ projekt.c
@@ -4,7 +4,10 @@
 
 // 11.12.2012 -- Pridal som printf
 
+// 11.12.2012 -- Upravena grafika 
+
 void main(){
     printf("Zaciname robit na projekte");
 	printf("Vitajte v programe Dodavka cez internet");
+	printf("===========================================================");
 }
